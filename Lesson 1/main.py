bc18bec1884ef3e3cc62dbc9dd2f0c1f25a96b08
Bryce <bbenjam5@kent.edu>
2019-12-18 19:37:15 -0500

    first python program

---
+++ Lesson 1/main.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = int(input("what is your age? "))
+year100 = str(100 - age)
+message = "\nYou will turn 100 in " + year100 + " years"
+print(message)
+number = int(input("Give me a number "))
+print(number * message)
