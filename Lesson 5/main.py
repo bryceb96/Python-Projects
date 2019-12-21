42be42e2c35d79f9aed9f8d74ee718fdb15d80e1
Bryce <bbenjam5@kent.edu>
2019-12-20 21:15:04 -0500

    made lesson 5

---
+++ Lesson 5/main.py
@@ -0,0 +1,15 @@
+import random
+
+a = random.sample(range(1,50), 10)
+b = random.sample(range(1,50), 10)
+c = []
+
+
+for x in a:
+    for y in b:
+        if x == y:
+            c.append(x)
+        
+
+for element in c:
+    print(element)
