7516598dabaad9ed2dd56fb7d89e9f8435fa7001
Bryce <bbenjam5@kent.edu>
2019-12-20 20:54:08 -0500

    update

--- Lesson 4/main.py
+++ Lesson 4/main.py
@@ -7,4 +7,4 @@ for element in x:
     if number % element == 0:
         print("element " + str(element) + " is a divisor of the number")
     else:
-        print("element " + str(element) + " is not a divisor of the number")
+        print("")
